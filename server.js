c0d68cc489f66b8ec4f8c7574ee3f895267c9336
Adnan <adnanvandyck@gmail.com>
2023-12-12 15:29:04 -0500

    adding places controller

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const PORT =process.env.PORT
 const app = express()
 
+app.use('/places', require('./controllers/places_controller'))
 
 app.get('/', (req, res) => {
     res.send('Peace to the World!')
