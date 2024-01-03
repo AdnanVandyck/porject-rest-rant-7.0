5da8c2c0c7a1c1987ddb2797f6a61f0678b4ba56
Adnan <adnanvandyck@gmail.com>
2024-01-02 23:33:17 -0500

    edited show page render

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,26 +7,27 @@ function show(data) {
   return (
     <Def>
       <main>
-        <div>
-          <h2>{data.place.name}</h2>
-          <h3>Rating</h3>
-          <p>Not Rated</p>
+        <div className="row">
+        <div className="col-sm-6">
+        <img src={data.place.pic} alt={data.place.name} />
+        <h3>
+          Located in {data.place.city}, {data.place.state}
+          </h3>
         </div>
-
-        <div>
-          <img src={data.place.pic} alt={data.place.name} />
-        </div>
-        <div>
-          <h3>Description</h3>
-          <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
-        </div>
-        <div>
-          <h3>Comments</h3>
-          <p>No comments yet!</p>
-        </div>
-
-
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+        <div className="col-sm-6">
+        <h1>{data.place.name}</h1>
+          <h2>Rating</h2>
+          <h3>Not Rated</h3>
+          <h2>
+            Description
+          </h2>
+          <h3>
+            {data.place.showEstablished()}
+          </h3>
+          <h4>
+            Serving {data.place.cuisines}
+          </h4>
+          <a href={`/places/${data.id}/edit`} className="btn btn-warning">
           Edit
         </a>
 
@@ -35,6 +36,12 @@ function show(data) {
             Delete
           </button>
         </form>
+        </div>
+        <div className="col-sm-6">
+          <h2>Comments</h2>
+          <h3>No comments yet!</h3>
+        </div>
+        </div>
       </main>
     </Def>
   );
